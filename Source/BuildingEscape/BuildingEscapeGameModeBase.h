c3707f28380fab85ac67d9fb64d24a2f53477b21
Sagar Doshi <doshi.sd@gmail.com>
2018-06-20 14:35:10 +0100

    Reorganised a number of content files. Also got audio working for door latch clicks on open and close. Nice, almost set for this environment.

--- Source/BuildingEscape/BuildingEscapeGameModeBase.h
+++ Source/BuildingEscape/BuildingEscapeGameModeBase.h
@@ -6,15 +6,9 @@
 #include "GameFramework/GameModeBase.h"
 #include "BuildingEscapeGameModeBase.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class BUILDINGESCAPE_API ABuildingEscapeGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
-	
-	
-	
 };
